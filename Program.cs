180284ad0b5bd12865244231f4e7068fc27624c2
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-10-10 22:21:33 -0300

    classes abstratas

--- Program.cs
+++ Program.cs
@@ -8,33 +8,39 @@ namespace Payments
         static void Main(string[] args)
         {
             //var customer = new Customer();
-           // customer.Name = "Teste";
-            Console.WriteLine("--------abc--------");
-            Payment P = new Payment();
-            P.Nome = "Filipe. Augusto. Santos.";
-            P.Idade = 20;
-          //  Console.WriteLine( "NOME " + P.Nome+ " Idade: "+P.Idade);
-            Movel m = new Movel(1);
-            Cama c = new Cama(5);
-            c.Preco();
+            // customer.Name = "Teste";
+            /*   Console.WriteLine("--------abc--------");
+              Payment P = new Payment();
+              P.Nome = "Filipe. Augusto. Santos.";
+              P.Idade = 20;
+              //  Console.WriteLine( "NOME " + P.Nome+ " Idade: "+P.Idade);
+              Movel m = new Movel(1);
+              Cama c = new Cama(5);
+              c.Preco(); */
+            var car = new Polo();
+            car.Ano = 2022;
+            car.Velocidade = 200.5m;
+            car.Nome = "polo preto 2012";
+            car.correr();
+            System.Console.WriteLine();
             //m.Preco();
             Console.ReadLine();
-           // ----------------------
-          // var pagamento = new  PaymentCreditCard();
-          // pagamento
+            // ----------------------
+            // var pagamento = new  PaymentCreditCard();
+            // pagamento
+        }
+    }
+    class Pagamento
+    {
+        //Garbage collector
+        public Pagamento()
+        {
+            Console.WriteLine("Iniciando o pagamento");
         }
     }
 
-    class Pagamento {
-            //Garbage collector
-            public Pagamento(){
-                Console.WriteLine("Iniciando o pagamento");
-            }
-        }
-
-
     #region construtor, metodos e propriedades
-    class Payment
+    partial class Payment
     {
         public DateTime vencimento { get; set; }
 
@@ -52,17 +58,20 @@ namespace Payments
                 _dataPagametno = value;
             }
         }
-        private string _nome; 
-        public string Nome {
-            get{
-                _nome = _nome.Replace(".","");
-              // Console.WriteLine("lendo valor");
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                _nome = _nome.Replace(".", "");
+                // Console.WriteLine("lendo valor");
                 return _nome;
             }
-            set{
+            set
+            {
                 _nome = value;
             }
-        } 
+        }
         //PROPG
         public int qualquer { get; private set; }
         //PROP FULL
@@ -70,46 +79,53 @@ namespace Payments
         public int Idade
         {
             get { return idade; }
-            set { 
-                idade = value+10; }
+            set
+            {
+                idade = value + 10;
+            }
         }
         //METODOS
         //nome do metodo é pagar
         //a assinatura do metodo é protected void Pagar(string numero) 
-        protected  virtual void Pagar(string numero) { }
+        protected virtual void Pagar(string numero) { }
         //sobrecarga - Quando cria metodos com assinaturas diferentes e nomes iguais 
-        protected void Pagar(string numero, DateTime vencimento, bool pagarAposVencimento = false) {
+        protected void Pagar(string numero, DateTime vencimento, bool pagarAposVencimento = false)
+        {
 
-         }
+        }
         private void ConsultarSaldoDoCartao()
         {
 
         }
     }
     //Sobrescrita
-    class Movel { 
-            //CONSTRUTOR
-            public DateTime  CriacaoDoMovel { get; set; }
-            public int Tamanho { get; set; }
+    class Movel
+    {
+        //CONSTRUTOR
+        public DateTime CriacaoDoMovel { get; set; }
+        public int Tamanho { get; set; }
         public Movel(int tamanho)
         {
             Tamanho = tamanho;
             CriacaoDoMovel = DateTime.Now;
-            Console.WriteLine("Iniciando o movel: "+ CriacaoDoMovel + "tamanho: " + Tamanho);
+            Console.WriteLine("Iniciando o movel: " + CriacaoDoMovel + "tamanho: " + Tamanho);
 
         }
-         
-        public virtual void Preco(){ Console.WriteLine("METODO PAI - MOVEL"); }
-         }
-    class Cama : Movel {
+
+        public virtual void Preco() { Console.WriteLine("METODO PAI - MOVEL"); }
+    }
+    class Cama : Movel
+    {
         public Cama(int tamanho) : base(tamanho)
         {
         }
 
-        public override void Preco(){ 
+        public override void Preco()
+        {
             base.Preco();//chamando metodo do pai
-            Console.WriteLine("METODO FILHO - CAMA");}
-         }
+            Console.WriteLine("METODO FILHO - CAMA");
+        }
+    }
     class PaymenTBoleto : Payment
     {
         void Teste()
@@ -117,28 +133,52 @@ namespace Payments
             base.Pagar("1");
         }
     }
-    class PaymentCreditCard : Payment{
-            //public override void Pagar( string numero){}
+    class PaymentCreditCard : Payment
+    {
+        //public override void Pagar( string numero){}
     }
     struct Customer
     {
         public string Name;
     }
     #endregion
-
-    
     public class Fuzilamento : IFuzilamento
     {
-        public int Quantidade { get ; set; }
+        public int Quantidade { get; set; }
 
         public void Atirar(string alvo)
         {
-            
+
         }
     }
-    public interface IFuzilamento{
-    public int Quantidade { get; set; }
-    public  void Atirar(string alvo);
-}
+    public interface IFuzilamento
+    {
+        public int Quantidade { get; set; }
+        public void Atirar(string alvo);
+    }
+
+
+    #region classe Abstrata e sobrescrita
+    abstract class Carro
+    {
+        public string Nome { get; set; }
+        public decimal Velocidade { get; set; }
+
+        public virtual void correr()
+        {
+            System.Console.WriteLine($"velocidade é {this.Velocidade}km/h");
+        }
+    }
+
+    class Polo : Carro
+    {
+        public int Ano { get; set; }
+        //sobrescrita
+        public override void correr()
+        {
+            System.Console.WriteLine($"velocidade do polo é {this.Velocidade}km/h");
+        }
+    }
+    #endregion
 
 }
